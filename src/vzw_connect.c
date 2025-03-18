4988c22e324506fa1a1f2656a66dc63807922e77
jforgue <code@jes.contact>
2025-03-18 16:18:18 -0400

    Clean up vzw_connect functions and fix buffer size

--- src/vzw_connect.c
+++ src/vzw_connect.c
@@ -38,7 +38,7 @@ static int extract_token(const char *src, char *dst, const char *token) {
   while (t < ret) {
     size_t token_len = (size_t)(tokens[t + 1].end - tokens[t + 1].start);
     if (jsoneq(src, &tokens[t], token)) {
-      (void)memcpy((void *)dst, (void *)(src + tokens[t + 1].start), token_len);
+      memcpy((void *)dst, (void *)(src + tokens[t + 1].start), token_len);
     } else if (jsoneq(src, &tokens[t], "error") ||
         jsoneq(src, &tokens[t], "errorCode")) {
       PRINTERR(
@@ -55,29 +55,23 @@ static int extract_token(const char *src, char *dst, const char *token) {
 
 #define OAUTH2_TOKEN_FIELD "Authorization:Basic "
 
-/** Base64 encoding is deterministic, so we know the length ahead of time + 1
- * for null terminator */
-#define ENC_LEN BASE64LEN(strlen(vzw_auth_keys)) + 1
-
-/** OAUTH2_TOKEN_FIELD length is 21 */
-#define OAUTH2_TOKEN_FIELD_SIZE strlen(OAUTH2_TOKEN_FIELD) + ENC_LEN
+/** Base64 encoding is deterministic, so we know the length ahead of time */
+#define OAUTH2_TOKEN_FIELD_SIZE sizeof(OAUTH2_TOKEN_FIELD) + BASE64LEN(strlen(vzw_auth_keys))
 
 int get_vzw_auth_token(const char *vzw_auth_keys, char *vzw_auth_token) {
+  char *ptr;
   CURL *curl = curl_easy_init();
   CURLcode res;
   struct curl_slist *headers = NULL;
   RecvData header_data = {NULL, 0};
   RecvData response_data = {NULL, 0};
-
-  char encoded_token[ENC_LEN];
   char auth_token_field[OAUTH2_TOKEN_FIELD_SIZE];
 
   curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
   curl_easy_setopt(curl, CURLOPT_POST, 1L);
   curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-  // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
   curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+  // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
   curl_easy_setopt(
       curl, CURLOPT_URL, "https://thingspace.verizon.com/api/ts/v1/oauth2/token"
@@ -88,12 +82,8 @@ int get_vzw_auth_token(const char *vzw_auth_keys, char *vzw_auth_token) {
   curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, heap_mem_write_callback);
   curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_data);
 
-  int base64StringLen =
-      base64(vzw_auth_keys, strlen(vzw_auth_keys), encoded_token);
-
-  (void)memcpy(auth_token_field, OAUTH2_TOKEN_FIELD, strlen(OAUTH2_TOKEN_FIELD));
-  (void)memcpy(&auth_token_field[strlen(OAUTH2_TOKEN_FIELD)], encoded_token, base64StringLen);
-  auth_token_field[OAUTH2_TOKEN_FIELD_SIZE - 1] = 0;
+  ptr = stpcpy(auth_token_field, OAUTH2_TOKEN_FIELD);
+  (void)base64(vzw_auth_keys, strlen(vzw_auth_keys), ptr);
 
   headers = curl_slist_append(headers, ACCEPT_JSON);
   headers = curl_slist_append(
@@ -127,8 +117,7 @@ fail:
 #define USERNAME_FIELD "\"username\":\""
 #define PASSWORD_FIELD "\",\"password\":\""
 #define ACCESS_TOKEN_FIELD "Authorization:Bearer "
-
-/** USERNAME_FIELD + PASSWORD_FIELD length + 4 for {"}0 */
+#define ACCESS_TOKEN_FIELD_SIZE(t) sizeof(ACCESS_TOKEN_FIELD) + strlen(t)
 #define LOGIN_FIELD_SIZE \
   sizeof(USERNAME_FIELD) + sizeof(PASSWORD_FIELD) + sizeof(VZW_USERNAME) + sizeof(VZW_PASSWORD)
 
@@ -136,6 +125,7 @@ int get_vzw_m2m_token(
     const char *username, const char *password, const char *vzw_auth_token,
     char *vzw_m2m_token
 ) {
+  char *ptr;
   CURL *curl = curl_easy_init();
   CURLcode res;
   struct curl_slist *headers = NULL;
@@ -143,20 +133,16 @@ int get_vzw_m2m_token(
   RecvData response_data = {NULL, 0};
 
   char post_field[LOGIN_FIELD_SIZE];
-  size_t access_token_field_size = strlen(ACCESS_TOKEN_FIELD) + strlen(vzw_auth_token) + 1;
-  char access_token_field[access_token_field_size];
+  char access_token_field[ACCESS_TOKEN_FIELD_SIZE(vzw_auth_token)];
 
   curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
   curl_easy_setopt(curl, CURLOPT_POST, 1L);
   curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-  // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
   curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
-  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+  // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
-  (void)memcpy(access_token_field, ACCESS_TOKEN_FIELD, strlen(ACCESS_TOKEN_FIELD));
-  (void
-  )memcpy(&access_token_field[strlen(ACCESS_TOKEN_FIELD)], vzw_auth_token, strlen(vzw_auth_token));
-  access_token_field[access_token_field_size - 1] = 0;
+  ptr = stpcpy(access_token_field, ACCESS_TOKEN_FIELD);
+  (void)stpcpy(ptr, vzw_auth_token);
 
   headers = curl_slist_append(headers, CONTENT_TYPE_JSON);
   headers = curl_slist_append(headers, ACCEPT_JSON);
@@ -173,22 +159,12 @@ int get_vzw_m2m_token(
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
 
   post_field[0] = '{';
-  (void)memcpy(&post_field[1], USERNAME_FIELD, strlen(USERNAME_FIELD));
-  (void
-  )memcpy(&post_field[strlen(USERNAME_FIELD) + 1], username, strlen(username));
-  (void)memcpy(
-      &post_field[strlen(USERNAME_FIELD) + 1 + strlen(username)],
-      PASSWORD_FIELD, strlen(PASSWORD_FIELD)
-  );
-  (void)memcpy(
-      &post_field
-          [strlen(USERNAME_FIELD) + 1 + strlen(username) +
-           strlen(PASSWORD_FIELD)],
-      password, strlen(password)
-  );
-  post_field[LOGIN_FIELD_SIZE - 3] = '"';
-  post_field[LOGIN_FIELD_SIZE - 2] = '}';
-  post_field[LOGIN_FIELD_SIZE - 1] = 0;
+  ptr = stpcpy(&post_field[1], USERNAME_FIELD);
+  ptr = stpcpy(ptr, username);
+  ptr = stpcpy(ptr, PASSWORD_FIELD);
+  ptr = stpcpy(ptr, password);
+  (void)stpcpy(ptr, "\"}");
+
   curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_field);
 
   res = curl_easy_perform(curl);
@@ -212,6 +188,7 @@ fail:
 }
 
 #define SESSION_TOKEN_FIELD "VZ-M2M-Token:"
+#define SESSION_TOKEN_FIELD_SIZE(t) sizeof(SESSION_TOKEN_FIELD) + strlen(t)
 
 #define DEVICE_ID_FIELD_START "\"deviceIds\":[{\"id\":\""
 #define DEVICE_ID_FIELD_END "\",\"kind\":\"MDN\"}],"
@@ -226,7 +203,7 @@ fail:
 
 #define SEND_NIDD_BODY_MAX_SIZE                                                              \
   sizeof(DEVICE_ID_FIELD_START) + sizeof(DEVICE_ID_FIELD_END) + sizeof(ACCOUNT_NAME_FIELD) + \
-      sizeof(MDT_FIELD) + sizeof(MESSAGE_FIELD) + MDN_SIZE + MAX_MDT_SIZE + MAX_MESSAGE_SIZE + 6
+      sizeof(MDT_FIELD) + sizeof(MESSAGE_FIELD) + MDN_SIZE + MAX_MDT_SIZE + MAX_MESSAGE_SIZE
 
 int send_nidd_data(char *vzw_auth_token, char *vzw_m2m_token, char *mdn, char *mdt, char *message) {
   char *ptr;
@@ -242,29 +219,20 @@ int send_nidd_data(char *vzw_auth_token, char *vzw_m2m_token, char *mdn, char *m
   }
 
   char post_field[SEND_NIDD_BODY_MAX_SIZE];
-
-  size_t access_token_field_size = strlen(ACCESS_TOKEN_FIELD) + strlen(vzw_auth_token) + 1;
-  char access_token_field[access_token_field_size];
-
-  size_t session_token_field_size = strlen(SESSION_TOKEN_FIELD) + strlen(vzw_m2m_token) + 1;
-  char session_token_field[session_token_field_size];
+  char access_token_field[ACCESS_TOKEN_FIELD_SIZE(vzw_auth_token)];
+  char session_token_field[SESSION_TOKEN_FIELD_SIZE(vzw_m2m_token)];
 
   curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
   curl_easy_setopt(curl, CURLOPT_POST, 1L);
   curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-  curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
   curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
-  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+  curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
-  (void)memcpy(access_token_field, ACCESS_TOKEN_FIELD, strlen(ACCESS_TOKEN_FIELD));
-  (void
-  )memcpy(&access_token_field[strlen(ACCESS_TOKEN_FIELD)], vzw_auth_token, strlen(vzw_auth_token));
-  access_token_field[access_token_field_size - 1] = 0;
+  ptr = stpcpy(access_token_field, ACCESS_TOKEN_FIELD);
+  (void)stpcpy(ptr, vzw_auth_token);
 
-  (void)memcpy(session_token_field, SESSION_TOKEN_FIELD, strlen(ACCESS_TOKEN_FIELD));
-  (void
-  )memcpy(&session_token_field[strlen(SESSION_TOKEN_FIELD)], vzw_m2m_token, strlen(vzw_m2m_token));
-  session_token_field[session_token_field_size - 1] = 0;
+  ptr = stpcpy(session_token_field, SESSION_TOKEN_FIELD);
+  (void)stpcpy(ptr, vzw_m2m_token);
 
   headers = curl_slist_append(headers, CONTENT_TYPE_JSON);
   headers = curl_slist_append(headers, ACCEPT_JSON);
@@ -281,7 +249,6 @@ int send_nidd_data(char *vzw_auth_token, char *vzw_m2m_token, char *mdn, char *m
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, heap_mem_write_callback);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
 
-  // char post_field[] = SEND_NIDD_BODY(mdn, mdt, message);
   post_field[0] = '{';
   ptr = stpcpy(&post_field[1], DEVICE_ID_FIELD_START);
   ptr = stpcpy(ptr, mdn);
@@ -293,8 +260,6 @@ int send_nidd_data(char *vzw_auth_token, char *vzw_m2m_token, char *mdn, char *m
   ptr = stpcpy(ptr, MESSAGE_FIELD);
   ptr += base64(message, strlen(message), ptr);
   (void)stpcpy(ptr, "\"}");
-
-  PRINTDBG("%s", post_field);
 
   curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_field);
 
